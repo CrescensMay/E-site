38e2897d112d7e1d8d75aa9b0eeac9b5c00d6081
Fidele <crescens.kob@outlook.fr>
2018-01-17 05:17:40 +0800

    get data videos thumbnails, title description and date and displayed them

--- out/artifacts/E_Site_war_exploded/js/search.js
+++ out/artifacts/E_Site_war_exploded/js/search.js
@@ -30,14 +30,49 @@ function searchVideo() {
             // maxResults: '25'
         },
         function (data) {
-            // var nextPageToken = data.nextPageToken;
-            // var prevPageToken = data.prevPageToken;
+            //go through paging
+            var nextPageToken = data.nextPageToken;
+            var prevPageToken = data.prevPageToken;
 
             console.log(data);
 
-            // $.each()
+            //looping through the items
+            $.each(data.items, function (i, item) {
+                //getting output
+                var outpout = getOutput(item);
+                //appending result
+                $results.append(outpout);
+            });
         }
     );
+}
+
+//building output
+function getOutput(item) {
+    var videoId = item.id.videoId;
+    var title = item.snippet.title;
+    var description = item.snippet.description;
+    var thumb = item.snippet.thumbnails.high.url;
+    var channelTitle = item.snippet.channelTitle;
+    var videoDate = item.snippet.publishedAt;
+
+    //Build ouptput string
+    var output = '<li>' +
+        '<div class="list-left">' +
+        '<img src="' + thumb + '">' +
+        '</div>' +
+        '<div class="list-right">' +
+        '<h3>' + title + '</h3>' +
+        '<small>By <span class="cTitle">' + channelTitle + ' </span> on ' + videoDate + '</small>' +
+        // '<p>' + description + '</p>' +
+        '</div>' +
+        '</li>' +
+        '<div class="clearfix"></div>' +
+        '';
+
+    return output;
+
+
 }
 
 //initialize API
